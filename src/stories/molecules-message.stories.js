9f58d47214992042265d6fa32fdc54622274b0d7
Gamechi666 <max.daisuke@ezweb.ne.jp>
2022-01-28 23:23:28 +0900

    edit

--- src/stories/molecules-message.stories.js
+++ src/stories/molecules-message.stories.js
@@ -1,5 +1,5 @@
-import Item_Message from "../components/molecules/molecules/message.html";
-import "../components/molecules/molecules/message.scss";
+import Item_Message from "../components/molecules/message.html";
+import "../components/molecules/message.scss";
 
 export default { title: "molecules/Message" };
 
