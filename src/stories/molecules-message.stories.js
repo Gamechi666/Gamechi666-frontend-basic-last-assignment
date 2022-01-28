4ff01cb3f6f796ffae1f259dbbec5ae17f11a5d7
Gamechi666 <max.daisuke@ezweb.ne.jp>
2022-01-28 23:11:22 +0900

    create molecules-message

---
+++ src/stories/molecules-message.stories.js
@@ -0,0 +1,6 @@
+import Item_Message from "../components/molecules/molecules/message.html";
+import "../components/molecules/molecules/message.scss";
+
+export default { title: "molecules/Message" };
+
+export const Message = () => Item_Message;
